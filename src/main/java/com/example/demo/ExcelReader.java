94c14e135dfa2f25d1f376896e159d6a5ae1ecf1
CK7-0909 <c0kalv01@gmail.com>
2025-01-29 20:34:50 -0500

    Works with excel format

--- src/main/java/com/example/demo/ExcelReader.java
+++ src/main/java/com/example/demo/ExcelReader.java
@@ -1,18 +1,15 @@
 package com.example.demo;
 
 import java.io.*;
-import java.sql.Date;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 
 @Component
 public class ExcelReader {
@@ -20,26 +17,20 @@ public class ExcelReader {
     public List<User> readExcelData(MultipartFile file) throws IOException {
 
         List<User> users = new ArrayList<>();
-        InputStream inputStream = file.getInputStream();
-        Workbook workbook = new XSSFWorkbook(inputStream);
-        Sheet sheet = workbook.getSheetAt(0);
-        Iterator<Row> rowIterator = sheet.iterator();
+        InputStreamReader inputStreamReader = new InputStreamReader(file.getInputStream());
+        //Workbook workbook = new XSSFWorkbook(inputStream);
+        //Sheet sheet = workbook.getSheetAt(0);
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(inputStreamReader);
 
-        // Skip header row
-        if (rowIterator.hasNext()) {
-            rowIterator.next();
-        }
-
-        while (rowIterator.hasNext()) {
-            Row row = rowIterator.next();
+        for (CSVRecord record : records) {
             User user = new User();
 
-            user.setfirstname(row.getCell(0).getStringCellValue());
-            user.setlastname(row.getCell(1).getStringCellValue());
-            user.setdob(row.getCell(2).getStringCellValue());
-            user.setBankBalance(row.getCell(3).getNumericCellValue());
-            user.setcountry(row.getCell(4).getStringCellValue());
-            user.setcreditAge(Integer.parseInt(row.getCell(5).getStringCellValue()));
+            user.setfirstname(record.get("FirstName"));
+            user.setlastname(record.get("LastName"));
+            user.setdob(record.get("DOB"));
+            user.setBankBalance(Double.parseDouble(record.get("BankBalance")));
+            user.setcountry(record.get("Country"));
+            user.setcreditAge(Integer.parseInt(record.get("CreditAge")));
             users.add(user);
             }
 
